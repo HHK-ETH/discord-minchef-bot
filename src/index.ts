1ef5341d25e2dc1c90bde5868dc414756aac12f3
HHK <hhk.contact@protonmail.com>
2022-07-18 15:34:09 +0200

    feat: export slash commands

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { ChainId, RPC, SUSHI_ADDRESS, MINICHEF_ADDRESS } from './constants';
+import { slashBalance, slashChains, slashRewards } from './commands';
 
 export { ChainId, RPC, SUSHI_ADDRESS, MINICHEF_ADDRESS };
+export { slashBalance, slashChains, slashRewards };
