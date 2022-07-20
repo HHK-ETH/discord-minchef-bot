f91c16128a02375d6b84333af4dc9c190788831e
HHK <hhk.contact@protonmail.com>
2022-07-20 15:48:36 +0200

    feat: export functions from src

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { ChainId, RPC, SUSHI_ADDRESS, MINICHEF_ADDRESS } from './constants';
 import { slashBalance, slashChains, slashRewards } from './commands';
+import StorageHelper from './storageHelper';
+import { checkBalanceRoutine } from './routines';
 
 export { ChainId, RPC, SUSHI_ADDRESS, MINICHEF_ADDRESS };
 export { slashBalance, slashChains, slashRewards };
+export { StorageHelper };
+export { checkBalanceRoutine };
