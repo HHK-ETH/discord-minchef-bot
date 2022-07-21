652ed877c5dbe38406311d7b6d54707fd3a4fb60
HHK <hhk.contact@protonmail.com>
2022-07-21 14:43:41 +0200

    feat: add minichef & multicall abi

--- imports/index.ts
+++ imports/index.ts
@@ -1,3 +1,7 @@
 import erc20 from './erc.json';
+import multicall from './multicall.json';
+import minichef from './minichef.json';
 
 export const ERC20_ABI = erc20;
+export const MULTICALL_ABI = multicall;
+export const MINICHEF_ABI = minichef;
